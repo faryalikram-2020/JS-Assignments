aca222b184b62057f014a2d06a9df732f5506e37
RIDA FARYAL <60549156+faryalikram-2020@users.noreply.github.com>
2020-07-23 23:23:08 +0500

    All   JS Chapters:
    CHAPTERS:1-67

---
+++ app.js
@@ -0,0 +1,69 @@
+//**********************************************CHAPTER NO 43-48**************************************************
+
+
+
+//Task #1:
+/*
+function class1(){
+	var cl = document.getElementById("clicks");
+	alert("this is website");
+}
+*/
+
+
+
+//Task #2:
+/*
+function alert1(){
+	var image = document.getElementById("one");
+	var image = document.getElementById("two");
+	var image = document.getElementById("three");
+	var image = document.getElementById("four");
+	alert("Thanks for purchasing a phone for us");
+
+}
+*/
+
+
+
+//Task #3:
+/*function remove(){
+	var row = document.getElementById("del").deleteRow(0);
+	
+}*/
+
+
+
+
+//Task#4:
+/*
+function firstimg(){
+	var mb = document.getElementById("pic")
+    mb.src="mb.1.jpg"
+
+	}
+
+
+function secondimg(){
+	var mb = document.getElementById("pic")
+    mb.src="mb.2.jpg"
+
+	}
+*/
+
+
+
+//Task#4:
+/*
+function add(){
+	var mb = document.getElementById("count");
+	mb.value++;
+	
+}
+
+function sub(){
+	var mb = document.getElementById("count");
+	mb.value--;
+	
+}
+*5
